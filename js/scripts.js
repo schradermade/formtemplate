b02c5c2d44b9cb38245cd0b6c9a122dabcfcf5e7
Nathan Schrader <nathanschrader@icloud.com>
2020-11-11 14:59:33 -0800

    add html form, beginning user&biz logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,36 @@
 // Business Logic
 
+function Tickets( customerName, movieTime, movieName )  {
+  this.customerName = customerName,
+  this.movieTime = movieTime,
+  this.movieName = movieName
+}
+
+Tickets.prototype.calculateCost = function()  {
+  let cost = 10;
+  if (this.customerName === "Christian") {
+    cost = 5;
+    return cost;
+  }
+  return cost;
+}
 
 // User Interface Logic
+
 $(document).ready(function() {
   $("#formOne").submit(function(event)  {
     event.preventDefault();
     
-    let firstName = $("#firstName").val();
-    $("#results").append(firstName);
+    let customerName = $("#customer-name").val();
+    let movieTime = $("#movie-time").val();
+    let movieName = $("#movie-name").val();
+    let movieTicket = new Tickets( customerName, movieTime, movieName );
+    let calculateCost = movieTicket.calculateCost();
+
+    console.log(movieTicket);
+    console.log(movieTicket.customerName, movieTicket.movieTime, movieTicket.movieName );
+    console.log(movieTicket.calculateCost());
+    $("#results").empty().append(customerName + " - $" + calculateCost);
   })
 });
 
